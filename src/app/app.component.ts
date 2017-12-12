7eec611218870c66df840b765a1c4682734e2539
Enver Dzhagarov <betterthanbestest@gmail.com>
2017-12-12 18:58:02 +0800

    feat: added  heroes component and leverages it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to Angular2 Lecture';
+  title = 'Angular2 Lecture';
 }
