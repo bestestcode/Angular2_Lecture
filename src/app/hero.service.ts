04d5d838d317c04797c06b227d71b7bc7b45a95f
Mobile Developer <betterthanbestest@gmail.com>
2017-12-18 18:56:46 +0800

    feat: used RxJS Observable to emit value asynchronously

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heros';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { Hero } from './Hero';
 
 @Injectable()
 export class HeroService {
 
   constructor() { }
 
-  getHeroes() {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,11 +12,10 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
 
   heroes: Hero[] = HEROES;
-  
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
